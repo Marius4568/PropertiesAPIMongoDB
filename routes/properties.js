eb4826cd04fb475ce23554e28f6899e0a243aa4a
Marius Urbonas <mariolee303@gmail.com>
2022-03-21 15:55:50 +0200

    test

--- routes/properties.js
+++ routes/properties.js
@@ -104,7 +104,7 @@ router.get("/countries", async (req, res) => {
 
       if (propertiesByCountry.length > 0) {
         console.log(propertiesByCountry);
-        res.send(propertiesByCountry);
+        res.send({ ...propertiesByCountry, limit: "2" });
       } else {
         res.status(404).send("no properties found");
       }
